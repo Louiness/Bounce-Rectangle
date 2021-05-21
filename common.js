7fab24f0dd37304d3700d0a32b41bbc8887a1fb0
Louiness <kks518@naver.com>
2021-05-21 17:29:55 +0900

    Updates

--- common.js
+++ common.js
@@ -1,3 +1,14 @@
 function extractValue(pageLength, max, min) {
     return Math.random() * ((pageLength - max) - min) + min;
 }
+function extractRadius(x, y) {
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+}
+function createDirection() {
+    const value = Math.random() - Math.random();
+    if(value < 0) {
+        return -1;
+    } else {
+        return 1;
+    }
+}
